d496d7195e864bb543a6dcdc8d8bd5511614621d
kchens <kkch3n@gmail.com>
2015-03-18 18:49:51 -0700

    adds user signed in?

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -10,13 +10,14 @@ describe Authenticable do
   describe "#current_user" do
     before do
       @user = FactoryGirl.create :user
-      request.headers['Authorization'] = @user.auth_token
+      request.headers["Authorization"] = @user.auth_token
       allow(authentication).to receive(:request).and_return(request)
     end
 
-    it "returns the user from the authorization header" do
+    it "returns the user from the header" do
       expect(authentication.current_user.auth_token).to eql @user.auth_token
     end
+
   end
 
   describe "#authenticate_with_token" do
@@ -35,5 +36,25 @@ describe Authenticable do
     end
 
     it { should respond_with 401 }
+  end
+
+  describe "#user_signed_in?" do
+    context "when there is a user on 'session'" do
+      before do
+        @user = FactoryGirl.create :user
+        allow(authentication).to receive(:current_user).and_return(@user)
+      end
+
+      it { should be_user_signed_in }
+    end
+
+    context "when there is no user on 'session'" do
+      before do
+        @user = FactoryGirl.create :user
+        allow(authentication).to receive(:current_user).and_return(nil)
+      end
+
+      it { should_not user_signed_in }
+    end
   end
 end
